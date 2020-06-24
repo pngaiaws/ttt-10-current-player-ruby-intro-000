92655371b3cb04b3594d169c7fd1a9e058d85f61
Pete Ngai <sendccme55@gmail.com>
2020-06-24 11:42:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,9 +3,9 @@ def turn_count(board)
   counter = 0
 
   board.each do |square|
-    if square == "X" || square == "O" 
+    if square == "X" || square == "O"
       counter += 1
     end
     counter
-    
+
 end
